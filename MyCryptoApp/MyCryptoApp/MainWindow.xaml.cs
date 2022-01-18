f91a8b2475ab17f29d629e58d64385701a040856
MaxMenenberg <61643885+MaxMenenberg@users.noreply.github.com>
2022-01-17 19:56:22 -0800

    Final AppVersion

--- MyCryptoApp/MyCryptoApp/MainWindow.xaml.cs
+++ MyCryptoApp/MyCryptoApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace MyCryptoApp
 {
@@ -23,6 +24,93 @@ namespace MyCryptoApp
 
         string plainText, cypherText;
         int r1Setting, r2Setting, r3Setting;
+        uint[] primes = new uint[3000];
+        uint mod, g, privateKey, publicKey, RpublicKey, ssKey;
+
+      
+
+        private void GenerateSecretKeyButton_Click(object sender, RoutedEventArgs e)
+        {
+            mod = (uint)Convert.ToInt32(modulusTextBox.Text);
+            g = (uint)Convert.ToInt32(baseTextBox.Text);
+
+            RpublicKey = (uint)Convert.ToInt32(recPulicKeyTextBox.Text);
+            privateKey = (uint)Convert.ToInt32(privateKeyTextBox.Text);
+            ssKey = modExp(RpublicKey, privateKey, mod);
+            sharedSecretKeyTextBox.Text = ssKey.ToString();
+
+            int sskLength = countDigits(ssKey);
+            char[] sskString = ssKey.ToString().ToCharArray();
+            string r1, r2, r3;
+            if (sskLength == 5)
+            {
+                r1 = sskString[0].ToString() + sskString[2].ToString();
+                r2 = sskString[2].ToString() + sskString[1].ToString();
+                r3 = sskString[4].ToString() + sskString[4].ToString();
+            }
+            else if (sskLength == 4) {
+                r1 = sskString[1].ToString() + sskString[1].ToString();
+                r2 = sskString[0].ToString() + sskString[2].ToString();
+                r3 = sskString[3].ToString() + sskString[3].ToString();
+            }
+
+            else if (sskLength == 3)
+            {
+                r1 = sskString[0].ToString() + sskString[1].ToString();
+                r2 = sskString[2].ToString() + sskString[1].ToString();
+                r3 = sskString[0].ToString() + sskString[2].ToString();
+            }
+
+            else if (sskLength == 2)
+            {
+                r1 = sskString[1].ToString() + sskString[1].ToString();
+                r2 = sskString[0].ToString() + sskString[1].ToString();
+                r3 = sskString[0].ToString() + sskString[0].ToString();
+            }
+
+            else{
+                r1 = sskString[0].ToString() + '1'.ToString();
+                r2 = sskString[2].ToString() + '5'.ToString();
+                r3 = sskString[0].ToString() + '7'.ToString();
+            }
+
+            r1TextBox.Text = r1;
+            r2TextBox.Text = r2;
+            r3TextBox.Text = r3;
+
+            R1SettingTextBox.Text = r1;
+            R2SettingTextBox.Text = r2;
+            R3SettingTextBox.Text = r3;
+
+
+        }
+
+
+        private void GeneratePublicKeyButton_Click(object sender, RoutedEventArgs e)
+        {
+            mod = (uint)Convert.ToInt32(modulusTextBox.Text);
+            g = (uint)Convert.ToInt32(baseTextBox.Text);
+
+            privateKey = (uint)Convert.ToInt32(privateKeyTextBox.Text);
+            publicKey = modExp(g, privateKey, mod);
+            publicKeyTextBox.Text = publicKey.ToString();
+        }
+
+
+        private void GenerateModButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random rng = new Random();
+            int randomIndex = rng.Next(0, 3000);
+
+            mod = primes[randomIndex];
+
+            g = (uint)rng.Next(2, (int)Math.Floor((Math.Pow(2, 32) - 1) / (mod - 1)));
+
+            modulusTextBox.Text = mod.ToString();
+            baseTextBox.Text = g.ToString();
+        }
+
+   
 
         private void EncryptButton_Click(object sender, RoutedEventArgs e)
         {
@@ -44,10 +132,42 @@ namespace MyCryptoApp
         public MainWindow()
         {
             InitializeComponent();
+            loadPrimes();
         }
 
         public int parseTextBoxString(string s) {
             return Int32.Parse(s);
         }
+
+        public void loadPrimes() {
+            int count = 0;
+            using (StreamReader sr = new StreamReader("primes.txt"))
+            {
+                while(!sr.EndOfStream){
+                    primes[count] = (uint)Convert.ToInt32(sr.ReadLine());
+                    count++;
+                }
+
+            }
+        
+        }
+
+        // y = a^b mod(p)
+        public static uint modExp(uint a, uint b, uint p)
+        {
+            uint y = 1;
+            for (int n = 0; n < b; n++)
+            {
+                y = (y * a) % p;
+            }
+
+            return y;
+        }
+
+        public int countDigits(uint x) {
+            string temp = x.ToString();
+            return temp.Length;
+        }
+
     }
 }
---
+++ PrimeParser/PrimeParser/PrimeParser/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PrimeParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeFile = "primes1.txt";
+            int[] primes = new int[1000000];
+            int[] primesSmallList = new int[3000];
+            int count = 0;
+            using (StreamReader sr = new StreamReader(primeFile)) {
+                while (!sr.EndOfStream) {
+                   
+                    string tempLine = sr.ReadLine();
+                    string[] tempLineItems = tempLine.Split(new char[] {' '});
+
+                    List<string> tempLineItems2 = new List<string>();
+                    for (int n = 0; n < tempLineItems.Length; n++) {
+                        if (tempLineItems[n] != "") {
+                            tempLineItems2.Add(tempLineItems[n]);
+                        }
+                    }
+
+                    for (int n = 0; n < tempLineItems2.Count - 1; n++) {
+                        string tempItem = tempLineItems2[n];
+                        int tempItemInt = Convert.ToInt32(tempItem);
+                        primes[count] = tempItemInt;
+                        count++;
+                    }
+
+                   
+                
+                }
+
+            }
+
+            for (int n = 2000; n < 5000; n++) {
+                primesSmallList[n-2000] = primes[n];
+
+            }
+            using (StreamWriter sw = new StreamWriter("primes.txt")) {
+                for (int n = 0; n < primesSmallList.Length; n++) {
+                    sw.WriteLine(primesSmallList[n].ToString());
+                }
+            }
+
+
+            int b = 1;
+        }
+    }
+}
